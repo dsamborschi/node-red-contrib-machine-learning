c58a8531a9437cbe5031b819631f73274378ba86
Dan Samborsky <dsamborschi@yahoo.com>
2025-07-06 18:12:20 -0400

    Update predictor.py

--- nodes/predictor/predictor.py
+++ nodes/predictor/predictor.py
@@ -1,33 +1,44 @@
 import json
-import pandas
+import pandas as pd
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../../utils')
 
-#read configurations
+# Read configurations
 config = json.loads(input())
 
 def load():
-	try:
-		from sklw import SKLW
-		return SKLW(path=config['path'])
-	except:
-		try:
-			from dnnctf import DNNCTF
-			return DNNCTF(path=config['path'], load=True)
-		except:
-			return None
+    try:
+        from sklw import SKLW
+        return SKLW(path=config['path'])
+    except:
+        try:
+            from dnnctf import DNNCTF
+            return DNNCTF(path=config['path'], load=True)
+        except:
+            return None
 
 model = load()
 
 while True:
-	#read request
-	features = pandas.read_json(input(), orient='values')
+    try:
+        # Read request
+        features = pd.read_json(input(), orient='values')
 
-	if model is None:
-		model = load()
-	if model is None:
-		raise('Cannot find model.')
-	model.update()
-	
-	print(json.dumps(model.predict(features)))
+        if model is None:
+            model = load()
+        if model is None:
+            raise Exception('Cannot find model.')
+
+        model.update()
+
+        prediction = model.predict(features)
+
+        # Safely convert to JSON serializable
+        if hasattr(prediction, 'tolist'):
+            prediction = prediction.tolist()
+
+        print(json.dumps(prediction))
+
+    except Exception as e:
+        print(json.dumps({ "error": str(e) }))
